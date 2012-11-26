b3448cdb89de1ffa56b26da3ded823f68b69551f
Nick Maloney <ngmaloney@gmail.com>
2012-11-26 10:19:56 -0500

    Added baseline postgres integration, needs to be a seperate recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,15 @@
 default[:freeradius][:install_method] = "package"
+default['freeradius']['dir'] = "/etc/freeradius"
+
+# Db vars
+default['freeradius']['db_type'] = "postgresql"
+default['freeradius']['db_server'] = "localhost"
+default['freeradius']['db_port'] = "5432"
+default['freeradius']['db_name'] = "radius"
+default['freeradius']['db_login'] = "radius"
+default['freeradius']['db_password'] = "radius"
+
+# Used for source installation
 default[:freeradius][:url] = "http://ftp.cc.uoc.gr/mirrors/ftp.freeradius.org/"
 default[:freeradius][:version] = "2.1.10"
 default[:freeradius][:checksum] = "b72d00d8d9c237b6bc3bfe89e6ccd99a7be63e699b305325ea60e04d5ddda4fe"
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "freeradius::#{node[:freeradius][:install_method]}"
+
+template "#{node['freeradius']['dir']}/sql.conf" do
+  source "sql.conf.erb"
+  owner "freerad"
+  group "freerad"
+  mode 0600
+  notifies :restart, 'service[freeradius]', :immediately
+end
+
+service "freeradius" do
+  supports :restart => true, :status => true, :reload => true
+  action [:enable, :start]
+end
